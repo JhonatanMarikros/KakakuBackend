dc77f6048f028ef4a6fb82373478169d828f3a17
JhonatanMarikros <jhonatanmarikros@gmail.com>
2024-06-18 12:36:07 +0700

    Update location.blade.php

--- resources/views/main/location.blade.php
+++ resources/views/main/location.blade.php
@@ -39,51 +39,6 @@
         @endforeach
     </div>
 
-    <footer class="site-footer">
-        <div class="container3">
-            <div class="row">
-                <div class="col-md-3">
-                    <!-- Bagian Delivery bagian footer -->
-                    <div class="delivery">
-                        <h3>Delivery Service</h3>
-                        <div class="image">
-                            <img src="{{ asset('image/gofood.jpg') }}" alt="GoFood">
-                            <img src="{{ asset('image/grabfood.jpg') }}" alt="GrabFood">
-                            <img src="{{ asset('image/shopee.jpg') }}" alt="Shopee">
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <!-- Bagian tentang kami bagian footer -->
-                    <div class="tentang-kami">
-                        <h3>Golden Grain</h3>
-                        <p>Jl. Letjen S. Parman No.1 3, RT.3/RW.8, Tomang, Kec. Grogol petamburan, Kota Jakarta Barat,
-                            Daerah Khusus Ibukota Jakarta 11440</p>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <!-- Bagian Delivery faq bagian footer -->
-                    <div class="faq">
-                        <h3>FAQ</h3>
-                        <p>Frequently Asked Question</p>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="social-media">
-                        <h3>Social Media</h3>
-                        <div class="social-icons">
-                            <span class="facebook"><i class="ri-facebook-line"></i></span>
-                            <span class="instagram"><i class="ri-instagram-line"></i></span>
-                        </div>
-                        <div class="by">
-                            @2024 Powered by Golden Grain.
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
-
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
