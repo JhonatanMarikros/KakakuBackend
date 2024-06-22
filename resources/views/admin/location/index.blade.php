b3528b1f4843c3f3045a87f6af9eb261ee690a52
Horizon2509 <randy.535220180@stu.untar.ac.id>
2024-06-22 14:43:31 +0700

    membuat style pada adminpage

--- resources/views/admin/location/index.blade.php
+++ resources/views/admin/location/index.blade.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="{{ asset('css/adminpage.css') }}">
 @extends('admin.layouts.main')
 @section('container')
 <div class="container">
---
+++ resources/views/admin/partials/sidebar.blade.php
@@ -0,0 +1,35 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'My Application')</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('css/adminpage.css') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=LXGW+WenKai+Mono+TC&display=swap" rel="stylesheet">
+</head>
+<div class="container">
+    <div class="row">
+        <div class="col-md-2 sidebar">
+            @auth('admin')
+                <span class="nav-link">Hello, {{ Auth::guard('admin')->user()->name }}</span>
+            @endauth
+            <a class="nav-link {{ $title === 'Admin Home' ? 'active' : '' }}" href="{{ route('admin.home') }}">Dashboard</a>
+            <a class="nav-link {{ $title === 'Users' ? 'active' : '' }}" href="{{ route('adminuser.index') }}">Users</a>
+            <a class="nav-link {{ $title === 'Admin' ? 'active' : '' }}" href="{{ route('admins.index') }}">Admin</a>
+            <a class="nav-link {{ $title === 'Makanan' ? 'active' : '' }}" href="/adminmakanan">Makanan</a>
+            <a class="nav-link {{ $title === 'Minuman' ? 'active' : '' }}" href="/adminminuman">Minuman</a>
+            <a class="nav-link {{ $title === 'Location' ? 'active' : '' }}" href="/adminlocations">Location</a>
+            <a class="nav-link {{ $title === 'Coupon' ? 'active' : '' }}" href="{{ route('admincoupon') }}">Coupon</a>
+            <div class="navbar-nav">
+                <form action="{{ route('admin.logout') }}" method="POST" class="d-inline">
+                    @csrf
+                    <button type="submit" class="btn btn-primary">
+                        <i class="ri-logout-box-r-line"></i> Logout
+                    </button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+sidebar.blade.php
